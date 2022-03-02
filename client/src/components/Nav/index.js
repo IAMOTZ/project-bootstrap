dff23c60f933f77aa5a8acf9b672f9335b029514
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2022-03-03 00:55:32 +0100

    client: Setup styled components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { AppBar, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
+import { NavContent } from './styled';
 
 const NavBar = () => {
   return (
     <AppBar position="static" sx={{ padding: '10px 0 10px 20px' }}>
-      <div className='nav-content'>
+      <NavContent>
         <Typography sx={{ margin: '10px 0 10px 20px', textDecoration: 'none' }} variant="h5" color="inherit" component={Link} to="/devices">
           Main Logo
         </Typography>
-      </div>
+      </NavContent>
     </AppBar>
   );
 }
