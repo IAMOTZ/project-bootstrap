128a578efb138827cfc85c4e5dffa0147f83c236
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2022-03-07 12:12:16 +0100

    server: Add prod build and start scripts

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
-import app from './src/app';
-import config from './src/config';
+import app from './app';
+import config from './config';
 
 export default app.listen(config.port, () => {
   console.log(`App listening on port ${config.port}`);
